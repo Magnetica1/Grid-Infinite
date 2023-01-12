5752c5bd3b4f367b0760c2d42372a8151f95b4bf
Magnetica1 <jackkw818@gmail.com>
2023-01-12 10:48:05 -0500

    Finish everything

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,21 @@
-    function scrollDuplicate() {
-        for (let index = 0; index < 1000; index++){
-            //Get the card
-            var card = document.querySelector("#card-1");
-            //Create the copy of card
-            const clone = card.cloneNode(true)    
-            //Add it to the DOM 
-            card.after(clone);
+window.addEventListener("scroll", function(event) {
+  
+    var top = this.scrollY
+
+        console.log(top);
+
+        if(top > 100) {
+            for (let index = 0; index < 99; index++){
+                //Get the card
+                var card = document.querySelector("#card-1");
+                //Create the copy of card
+                const clone = card.cloneNode(true)    
+                //Add it to the DOM 
+                card.after(clone);
+            }
+            for (let x = 0; x < 98; x++) {
+            //Removes element
+            card.remove()
+            }
         }
-    }
-
-    scrollDuplicate()
-
-const scroller = document.querySelector("#scroller");
-
-scroller.addEventListener("scroll", (event) => {
-  console.log(scroller.scrollTop);
-});
+}, false);
